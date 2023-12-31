401448427efd325a789afe0acb7af2c1165c8b61
ayaan-d <ayaan0145@gmail.com>
2023-12-30 20:56:12 -0500

    add mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
+const { mongoose } = require('mongoose');
 
 const app = express();
-
 const port = 8000;
+
+//database connect
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log('Database connected'))
+.catch((err) => console.log('Database not connected', err))
+
+
+app.use(express.json())
 
 app.use('/', require('./routes/authRoutes'))
 
