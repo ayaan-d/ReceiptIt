13da7a17528a87920ba64590a9ae344ca8f967c2
ayaan-d <ayaan0145@gmail.com>
2024-01-04 16:41:28 -0500

    usercontext and update main

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const { mongoose } = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 8000;
@@ -12,9 +13,10 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((err) => console.log('Database not connected', err))
 
 
-app.use(express.json())
-
-app.use('/', require('./routes/authRoutes'))
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
+app.use('/', require('./routes/authRoutes'));
 
 app.listen(port, () => console.log('Server is running on port 8000'));
 
