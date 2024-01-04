588032241d1aef0d5b4483e8dd856734f107b9aa
ayaan-d <ayaan0145@gmail.com>
2024-01-04 16:40:04 -0500

    pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,22 +4,27 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Upload from './pages/Upload';
-import axios from 'axios'
+//import Upload from './pages/Upload';
+import Dashboard from './pages/Dashboard'
+import axios from 'axios';
+import { Toaster } from 'react-hot-toast';
+import {UserContextProvider} from './context/userContext';
 
-axios.default.baseURL = 'http://localhost:8000'
+axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <>
+    <UserContextProvider>
     <Navbar />
+    <Toaster position='bottom-right' toastOptions={{duration: 2000}} />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/register' element={<Register />} />
       <Route path='/login' element={<Login />} />
-      <Route path='/upload' element={<Upload />} />
+      <Route path='/dashboard' element={<Dashboard />} />
     </Routes>
-    </>
+    </UserContextProvider>
   )
 }
 
