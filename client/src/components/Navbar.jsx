6d168c74e472e227b558344f11577ed73c5aea1f
Mizna <ayaan0145@gmail.com>
2024-01-07 13:33:54 -0500

    Initial UI work

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,40 @@
 import { Link } from 'react-router-dom';
+// import * as style from "./Navbar";
+import {
+  // Card,
+  Flex,
+  // Metric,
+  ProgressBar,
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Text,
+} from "@tremor/react";
+
+import "../pages/Home.css"
+
 
 export default function Navbar() {
+
   return (
-    <nav>
-        <Link to='/'> Home </Link>
-        <Link to='/login'> Login </Link>
-        <Link to='/register'> Register </Link>
-        <Link to='/upload'> Upload </Link>
-    </nav>
+<nav id="navbar" className="">
+   <div className="nav-wrapper">
+      {/* <div className="logo">
+         <img className="logo" src="https://cdn.glitch.me/4138ce3b-24ae-49a3-bd13-8f07d69684f5/b66ab14de4dccbb0fb5a28ca79ed81c0-justice-scale-circle-icon.png?v=1639814780739" height="40px" 
+        //  <input onClick="document.getElementById('landing').scrollIntoView();" />
+      </div> */}
+
+<TabGroup>
+        <TabList className="mt-8">
+          <Link className="link" to='/'><Tab>Home</Tab></Link>
+          <Link className="link" to='/login'><Tab>Login</Tab></Link>
+          <Link className="link" to='/register'><Tab>Register</Tab></Link>
+          <Link className="link" to='/upload'><Tab>Upload</Tab></Link>
+        </TabList>
+      </TabGroup>
+   </div>
+</nav>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,57 @@
 
+import "./Home.css";
+import Grid from '@mui/material/Grid';
+import ReceiptSVG from "../../assets/receipt-animate.svg"
+import { Link } from 'react-router-dom';
+import {
+  // Card,
+  Flex,
+  Col,
+  // Metric,
+  ProgressBar,
+  Tab,
+  TabGroup,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from "@tremor/react";
+import { Metric, Title, Subtitle, Bold, Italic, Text } from "@tremor/react";
+
+
+import { Image } from "react-bootstrap";
+
 export default function Home() {
   return (
     <div>
-      Home
+      <header id="landing">
+        <Grid container>
+          <Grid id="picdiv" item xs={3}>
+            <img id="pic" src={ReceiptSVG}></img>
+          </Grid>
+          <Grid item xs={6}>
+            <Title id="title">Welcome To ReceiptIt</Title>
+            <Subtitle id="subtitle" style={{ marginBottom: "1%"}}><Italic>Advice You Can Bank On</Italic></Subtitle>
+            <div style={{ marginTop: "2vh"}}>
+              <a className="button" href="/login"> Login</a>
+              <a className="button" href="/register" style={{ marginLeft: "2%" }}>Register</a>
+
+            </div>
+          </Grid>
+        </Grid>
+      </header>
+
+      {/* <div id="title">
+   <h1 style="margin-block-start: 0; margin-block-end: 0;"> About Us</h1>
+   </div>
+  
+<section id="section">
+   <div className="container">
+      <b> "Become More Than An Investor, Become An Owner With Verve Today"</b>
+   </div>
+   <p> Verve brings financial security and sustainable outcomes together through affordable investment opportunities for clients.
+     At Verve, we believe in creating a diverse and inclusive environment fostering investors, technologists and team members to allow for customer satisfaction. Learn more about why you should choose Verve below.   </p>
+</section> */}
     </div>
   )
 }
+
